60feccf56efe998f396a25f9d61752c39f96bb61
hmurich <hmirich@mail.ru>
2017-03-30 18:20:41 +0600

    check point in  Area

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,8 +7,26 @@ use Cache;
 use App\User;
 use Hash;
 use App\Model\SysDirectoryName;
+use App\Model\Generators\PointInArea;
 
 class TestController extends Controller{
+    function getPointInArea () {
+        $pointLocation = new PointInArea();
+
+        $points = array("57.5321 38.2670", '57.5244 38.1008', '57.5557 38.1825', '57.6254 38.1461', '57.5257 38.3212');
+        $polygon = array("57.5465 38.2148","57.5834 38.2491","57.5867 38.3411",
+                        "57.5240 38.4276","57.4774 38.3665","57.4659 38.2175",
+                        "57.5229 38.1461", "57.5465 38.2148");
+        // The last point's coordinates must be the same as the first one's, to "close the loop"
+        foreach($points as $key => $point) {
+            if ($pointLocation->pointInPolygon($point, $polygon))
+                echo "point " . ($key+1) . " ($point): внутри <br/>";
+            else
+                echo "point " . ($key+1) . " ($point): снаружи  <br/>";
+        }
+
+    }
+
     function getOrderSession(){
         session()->put('order.1.1.count', 100);
         session()->put('order.1.1.cost', 300);
