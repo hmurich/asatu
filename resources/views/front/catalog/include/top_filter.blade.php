66ec7d928d1f84456a608c02b15945443e75a3d1
sainsemey <sainastana@gmail.com>
2017-04-05 18:49:12 +0600

    cc

--- resources/views/front/catalog/include/top_filter.blade.php
+++ resources/views/front/catalog/include/top_filter.blade.php
@@ -2,14 +2,14 @@
     <form action='{{ action('Front\CatalogController@postAddress') }}' method="post">
         <div class="restaurants-filtr__item restaurants-filtr__item-city">
             <span>{{ $location->address }}</span>
-            <a href='#modal_change_address' class="button restaurants-filtr__button--city open_modal" type='button'>
+            <a href='#modal_change_address' class="button restaurants-filtr__button--city open_modal" >
                 изменить
             </a>
         </div>
     </form>
 
     <form action='{{ action('Front\CatalogController@getList') }}' method="get">
-        <div class="restaurants-filtr__item ">
+        <div class="restaurants-filtr__item restaurants-filtr__item--bot">
             <input type="text" name='name' value='{{ (isset($ar_input["name"]) ? $ar_input["name"] : null) }}' placeholder="Введите ресторан	" required="">
             <button class="button restaurants-filtr__button" type='submit'>
                 найти
