3c5405a284163e49153f4c7919606f2fc221e87a
hmurich <hmirich@mail.ru>
2017-04-05 14:31:42 +0600

    fix

--- resources/views/restoran/order/include/cancel_dialog.blade.php
+++ resources/views/restoran/order/include/cancel_dialog.blade.php
@@ -1,13 +1,15 @@
-<div id="modal2222" class="modal_div"> <!-- скрытый див с уникальным id = modal1 -->
-    <span class="modal_close"></span>
-	<div class="modal-title">Выберите причину отказа ?</div>
-	<ul class="cancel-list">
-        @foreach ($ar_close_ar as $id=>$name)
-            <li>
-                <a href="{{ action('Restoran\OrderController@getChangeStatus', array($order->id, $status_cancel, $id)) }}" >
-                    {{ $name }}
-                </a>
-            </li>
-        @endforeach
-	</ul>
-</div>
+@if ($order)
+    <div id="modal2222" class="modal_div"> <!-- скрытый див с уникальным id = modal1 -->
+        <span class="modal_close"></span>
+    	<div class="modal-title">Выберите причину отказа ?</div>
+    	<ul class="cancel-list">
+            @foreach ($ar_close_ar as $id=>$name)
+                <li>
+                    <a href="{{ action('Restoran\OrderController@getChangeStatus', array($order->id, $status_cancel, $id)) }}" >
+                        {{ $name }}
+                    </a>
+                </li>
+            @endforeach
+    	</ul>
+    </div>
+@endif
