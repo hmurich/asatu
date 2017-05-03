6173ddcef0238569aad52e97a564d1eee755efeb
hmurich <hmirich@mail.ru>
2017-05-03 15:28:52 +0600

    fix menu active

--- resources/views/admin/include/menu.blade.php
+++ resources/views/admin/include/menu.blade.php
@@ -1,55 +1,55 @@
 <ul class="admin-nav">
     @if (Auth::user() && Auth::user()->type_id == 1)
-        <li class='{{ (Request::is('adminka/menu-type') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/menu-type*') ? "active" : null) }}'>
             <a href="{{ action('Admin\MenuTypeController@getIndex') }}">Виды блюд</a>
         </li>
-        <li class='{{ (Request::is('adminka/kitchen') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/kitchen*') ? "active" : null) }}'>
             <a href="{{ action('Admin\KitchenController@getIndex') }}">Кухни</a>
         </li>
-        <li class='{{ (Request::is('adminka/city') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/city*') ? "active" : null) }}'>
             <a href="{{ action('Admin\CityController@getIndex') }}">Города</a>
         </li>
-        <li class='{{ (Request::is('adminka/page') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/page*') ? "active" : null) }}'>
             <a href="{{ action('Admin\PageController@getIndex') }}">Cтраницы</a>
         </li>
-        <li class='{{ (Request::is('adminka/static-page') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/static-page*') ? "active" : null) }}'>
             <a href="{{ action('Admin\StaticPageController@getIndex') }}">Стат. страницы</a>
         </li>
-        <li class='{{ (Request::is('adminka/moderator') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/moderator*') ? "active" : null) }}'>
             <a href="{{ action('Admin\ModeratorController@getIndex') }}">Модераторы</a>
         </li>
-        <li class='{{ (Request::is('adminka/translator') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/translator*') ? "active" : null) }}'>
             <a href="{{ action('Admin\TranslateController@getIndex') }}">Переводы</a>
         </li>
-        <li class='{{ (Request::is('adminka/site-setting') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/site-setting*') ? "active" : null) }}'>
             <a href="{{ action('Admin\SiteSettingController@getIndex') }}">Настройки</a>
         </li>
     @elseif (Auth::user() && Auth::user()->type_id == 2)
-        <li class='{{ (Request::is('adminka/order/list') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/order/list*') ? "active" : null) }}'>
             <a href="{{ action('Admin\OrderController@getList') }}">заказы</a>
         </li>
-        <li class='{{ (Request::is('adminka/restoran') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/restoran*') || Request::is('adminka/menu*') || Request::is('adminka/location*') ? "active" : null) }}'>
             <a href="{{ action('Admin\RestoranController@getIndex') }}">рестораны</a>
         </li>
-        <li class='{{ (Request::is('adminka/sale') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/sale*') ? "active" : null) }}'>
             <a href="{{ action('Admin\SaleController@getIndex') }}">акции</a>
         </li>
-        <li class='{{ (Request::is('adminka/promo/list') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/promo/list*') ? "active" : null) }}'>
             <a href="{{ action('Admin\PromoController@getList') }}">промо коды</a>
         </li>
-        <li class='{{ (Request::is('adminka/history/list') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/history/list*') ? "active" : null) }}'>
             <a href="{{ action('Admin\HistoryController@getList') }}">трекинг</a>
         </li>
-        <li class='{{ (Request::is('adminka/client/list') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/client/list*') ? "active" : null) }}'>
             <a href="{{ action('Admin\ClientController@getList') }}">клиенты</a>
         </li>
-        <li class='{{ (Request::is('adminka/registr-restoran/list') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/registr-restoran/list*') ? "active" : null) }}'>
             <a href="{{ action('Admin\RegistrRestoranController@getList') }}">заявки</a>
         </li>
-        <li class='{{ (Request::is('adminka/ticket/list') ? "active" : null) }}'>
+        <li class='{{ (Request::is('adminka/ticket/list*') ? "active" : null) }}'>
             <a href="{{ action('Admin\TicketController@getList') }}">запросы</a>
         </li>
-        <li class='{{ (Request::is('moderator/cabinet') ? "active" : null) }}'>
+        <li class='{{ (Request::is('moderator/cabinet*') ? "active" : null) }}'>
             <a href="{{ action('Moderator\CabinetController@getCabinet') }}">Кабинет</a>
         </li>
     @endif
--- resources/views/restoran/include/menu.blade.php
+++ resources/views/restoran/include/menu.blade.php
@@ -1,11 +1,11 @@
 <ul class="admin-nav">
-    <li class='{{ (Request::is('cabinet/order/list') ? "active" : null) }}'>
+    <li class='{{ (Request::is('cabinet/order/list*') ? "active" : null) }}'>
         <a href="{{ action('Restoran\OrderController@getList') }}">ОБРАБОТКА ЗАКАЗА</a>
     </li>
-    <li class='{{ (Request::is('cabinet/review/list') ? "active" : null) }}'>
+    <li class='{{ (Request::is('cabinet/review/list*') ? "active" : null) }}'>
         <a href="{{ action('Restoran\ReviewController@getList') }}">ОТЗЫВЫ</a>
     </li>
-    <li class='{{ (Request::is('cabinet/menu/list') ? "active" : null) }}'>
+    <li class='{{ (Request::is('cabinet/menu/list*') ? "active" : null) }}'>
         <a href="{{ action('Restoran\MenuController@getList') }}">БЛЮДА</a>
     </li>
     <li >
@@ -17,10 +17,10 @@
             @endif
         </a>
     </li>
-    <li class='{{ (Request::is('cabinet/history/list') ? "active" : null) }}'>
+    <li class='{{ (Request::is('cabinet/history/list*') ? "active" : null) }}'>
         <a href="{{ action('Restoran\HistoryController@getList') }}">ИСТОРИЯ</a>
     </li>
-    <li class='{{ (Request::is('cabinet/ticket/list') ? "active" : null) }}'>
+    <li class='{{ (Request::is('cabinet/ticket/list*') ? "active" : null) }}'>
         <a href="{{ action('Restoran\TicketController@getList') }}">ЗАПРОСЫ</a>
     </li>
 </ul>
